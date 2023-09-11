c9510b7a6b37e946af1feadd20ba66f38dfb5fd3
Devapatel0603 <142797423+Devapatel0603@users.noreply.github.com>
2023-09-11 10:34:28 +0000

    Change Practical_1_10.py and add Practical_1_10,=.txt

---
+++ Practical_1_10.py
@@ -0,0 +1,13 @@
+def main():
+    with open("Practical_1_10.txt") as file:
+        count = 0
+        pds_count = 0
+        for row in file:
+            count = count+1
+            if(row.startswith("PDS")):
+                pds_count = pds_count + 1
+    print("Number of lines :",count)
+    print("Number of lines which starts with 'PDS' :",pds_count)
+
+if __name__ == "__main__":
+    main()
