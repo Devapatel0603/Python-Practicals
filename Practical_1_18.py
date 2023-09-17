fbcb9eca088634d688ed20f5809d87d7d33c47aa
Devapatel0603 <142797423+Devapatel0603@users.noreply.github.com>
2023-09-17 07:09:07 +0000

    Update Practical_1_18.py

--- Practical_1_18.py
+++ Practical_1_18.py
@@ -3,7 +3,6 @@ def main():
     n = int(input("Enter number of students : "))
     for _ in range(n):
         ins_details(stud_list)
-    # print(stud_list)
     print(sort_spi(stud_list))
 
 def ins_details(dic):
@@ -17,10 +16,6 @@ def ins_details(dic):
     dic.append(dict)
 
 def sort_spi(spi_list):
-    # for i in spi_list:
-        # spi_list[i]["SPI"].sort()
-    #     i["SPI"].sort()
-    # return(spi_list)
     print(sorted(spi_list,key = lambda i:i["SPI"]))
             
 if __name__ == "__main__":
