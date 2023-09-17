95e4092e910ef84495465dd33d608b118316b7c4
Devapatel0603 <142797423+Devapatel0603@users.noreply.github.com>
2023-09-17 07:45:00 +0000

    Update Practical_1_7.py

--- Practical_1_7.py
+++ Practical_1_7.py
@@ -1,5 +1,17 @@
-n = 5
- 
-for i in range(1, n+1):
-    for j in range(0, n-i+1):
-        print(' ', end='')
+def pascal(n):
+    coeff=[[0]*(n+1) for _ in range(n)]
+    for i in range(n):
+        for j in range(i+1):
+            if j==0:
+                coeff[i][j]=1
+            else:
+                coeff[i][j]=coeff[i-1][j-1]+coeff[i-1][j]
+
+    # Print the coefficients in triangle format
+    for i in range(n):
+        for j in range(i+1):
+            print(coeff[i][j],end=" ")
+        print("")
+    
+n = int(input("Enter a number : "))
+pascal(n)
