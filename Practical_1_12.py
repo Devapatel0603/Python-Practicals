3f361458e0e4ccf66b5d13a7d686c2be295cf204
Devapatel0603 <142797423+Devapatel0603@users.noreply.github.com>
2023-09-17 14:32:32 +0530

    Update Practical_1_12.py

--- Practical_1_12.py
+++ Practical_1_12.py
@@ -4,7 +4,8 @@ def list_sort(list,n,c):
 list = [["java", 1995], ["c++", 1983], ["python", 1989]] 
 list_sort(list,3,1)
 print(list)
-print("smallest : ",min(list))
-print("Largest : ",list[len(list)-1])
-num_list = [3,328,48,38,3827]
-print("Sum : ",sum(num_list))
+num_list = [3, 328, 48, 38, 3827]
+print("Number list : ",num_list)
+print("Smallest : ",min(num_list))
+print("Largest: ",max(num_list))
+print("Addition of all members : ",sum(num_list))
